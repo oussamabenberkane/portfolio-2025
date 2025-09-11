49ca3d3ef21b3ac6dce69c1ebb987b6a3f962d80
oussama benberkane <oussamabenberkane.pro@gmail.com>
2025-09-11 18:34:39 +0200

    feat(section): added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './sections/About'
 import Projects from './sections/Projects'
 import Experiences from './sections/Experiences'
 import Testimonial from './sections/Testimonial'
+import Contact from './sections/Contact'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Experiences />
       <Projects />
       <Testimonial />
+      <Contact />
       <section className='min-h-screen'></section>
       {/* <About />
       <Projects />
